d6f70f4b45dd124f7bd51b8f74268a5b0adf239f
ssharifm33 <samsharifmiller@gmail.com>
2024-10-12 10:18:48 -0400

    pls work

---
+++ run-club/src/components/RunItem.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function RunItem({ run }) {
+  return (
+    <div className="card h-100">
+      <div className="card-header text-white bg-dark">
+        {new Date(run.date).toLocaleDateString()}
+      </div>
+      <div className="card-body">
+        <p className="card-text">
+          <strong>Distance:</strong> {run.distance} km<br />
+          <strong>Time:</strong> {run.time}
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default RunItem;
